89a4955fecaec3e29bdb93f49eb7b3e6bd28f844
samer25 <sammy-codee@gmail.com>
2020-12-14 02:17:55 +0200

    adding form in html and content

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,20 @@
-alert('works')
+$(document).ready(function () {
+    console.log("hello")
+    // First thing the users sees
+    const elementBox = document.getElementById("box");
+    const elementArtifact = document.getElementById("artefact");
+    const elementPhilosophy = document.getElementById("philosophy-content");
+    elementArtifact.hidden = hideAll
+    elementBox.scrollIntoView();
+
+
+});
+
+function scrollFunction(id) {
+    $("body,html").animate(
+        {
+            scrollTop: $(id).offset().top
+        },
+        800 //speed
+    );
+}
